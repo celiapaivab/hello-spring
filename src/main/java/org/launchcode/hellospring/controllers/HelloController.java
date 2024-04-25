c398de7e3b38c54a26946444de095cbd79482889
celiapaivab <celiadepaivabruno@gmail.com>
2024-04-25 15:53:35 -0500

    creating new branch

--- src/main/java/org/launchcode/hellospring/controllers/HelloController.java
+++ src/main/java/org/launchcode/hellospring/controllers/HelloController.java
@@ -4,6 +4,8 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 @Controller
+@ResponseBody
+@RequestMapping("hello")
 public class HelloController {
 
     // Handles request at path /hello
@@ -13,26 +15,26 @@ public class HelloController {
 //        return "Hello, Spring!";
 //    }
 
+    // lives /hello/goodbye
     @GetMapping("goodbye")
-    @ResponseBody
     public String goodbye() {
         return "Goodbye, Spring!";
     }
 
+    // lives /hello/hello
     // Responds to get requests at /hello?name=LaunchCode
-    @RequestMapping(value = "hello", method = {RequestMethod.GET, RequestMethod.POST})
-    @ResponseBody
+    @RequestMapping(method = {RequestMethod.GET, RequestMethod.POST})
     public String helloWithQueryParam(@RequestParam String name) {
         return "Hello, " + name + "!";
     }
 
     // Responds to get requests at /hello/LaunchCode
-    @GetMapping("hello/{name}")
-    @ResponseBody
+    @GetMapping("{name}")
     public String helloWithPathParam(@PathVariable String name) {
         return "Hello, " + name + "!";
     }
 
+    // lives /hello/form
     @GetMapping("form")
     @ResponseBody
     public String helloForm() {
